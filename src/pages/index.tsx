6915bbe1d3ddd73ab3130365ac3e55e1458b900a
hironorioka28 <hironorioka28@gmail.com>
2022-03-07 16:21:39 +0900

    add Tailwind CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <h1>Hello Three.js</h1>
+  return (
+    <div className="bg-indigo-900">
+      <h1 className="text-9xl text-amber-300 font-bold">Hello Three.js</h1>
+    </div>
+  )
 }
 
 export default Home
